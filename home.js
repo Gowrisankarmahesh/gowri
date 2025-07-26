883708d769693be8b8b481356313d6a985121d05
gowrisankarMahesh <gowrisankarcse6774@gmail.com>
2025-07-26 15:37:51 +0530

    created a new html file

---
+++ home.js
@@ -0,0 +1,77 @@
+// var a=10;
+// var b="11";
+// let s1="hi";
+// let s2="Hi";
+// let x=true;
+// let y=true;
+// if(x!=y)
+// {
+// console.log("condition true");
+// }
+// else{
+//     console.log("conditon false");
+// }
+// console.log(s1!=s2);
+// console.log(s1!==s2);
+// console.log(a!=b);
+// console.log(a%b);
+
+
+// let num=123;
+// let str="123";
+// console.log(num!=str);
+// function max1(mark){
+//     if(mark>30){
+//         console.log(mark+"it is A grade");
+//     }
+//     else if(mark <40 && mark >=50)
+//     {
+//         console.log(mark+"this a B grade mark");
+//     }
+//     else if(mark <60 && mark >70)
+//     {
+//         console.log(mark+"This is a C grade mark");
+//     }
+//     else
+//         {
+//         console.log(mark+ "it is fail mark")
+//     }
+// }
+// max1(30)
+
+// function canc(num1,num2,operand){
+// switch(operand){
+//     case "+" :
+//         console.log(num1+num2);
+//         break;
+
+// case "-":
+//     console.log(num1-num2);
+//     break;
+//     case "%":
+//         console.log(num1%num2)
+//         break
+
+//     default :
+//     console.log(operand,"pls enter the vaild password")
+// }}
+// canc(30,22,'!')
+
+//  function countAllCharacters(s) {
+//   const counts = {};
+//   for (const c of s) {
+//     counts[c] = (counts[c] || 0) + 1;
+//   }
+//   return counts;
+// }
+
+// Example usage:
+// const input = "Gowrisankar";
+// console.log(countAllCharacters(input));
+// let s=[1,2,3,4,5,6]
+// console.log(s.splice(2,3))
+
+
+let name1="hi world";
+console.log(name1);
+
